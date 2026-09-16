305f1d2372e28363b7840965476c00ce8e55b68a
zxlim <zixinlim862@gmail.com>
2026-09-16 21:25:04 +0800

    Add merge_sort function with comparison counting
    Implemented the merge_sort function to sort an array and count comparisons.

--- src/sorting/merge_sort.py
+++ src/sorting/merge_sort.py
@@ -1,2 +1,28 @@
-def merge_sort():
-    return
+def merge_sort(A, left, mid, right):  # merge sort from lecture
+    comparisons = 0
+    i = 0
+    m = mid - left
+    k = left
+    # Copy A[left] to A[right] into an array B 
+    B = A[left : right + 1]
+    
+    # j loops from the start of the right half (m + 1) to the end of B
+    for j in range(m + 1, len(B)):
+        while i <= m:
+            comparisons += 1 
+            if B[i] <= B[j]:
+                A[k] = B[i]
+                i += 1
+                k += 1
+            else:
+                break 
+                
+        A[k] = B[j]
+        k += 1
+        
+    while i <= m:
+        A[k] = B[i]
+        i += 1
+        k += 1
+        
+    return comparisons
