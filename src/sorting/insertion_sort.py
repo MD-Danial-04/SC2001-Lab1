0113e02cadc58924e330a9059a7f4d49b3c46f3f
zxlim <zixinlim862@gmail.com>
2026-09-16 19:23:35 +0800

    Add insertion_sort function
    Implement insertion sort algorithm with comparison count.

--- src/sorting/insertion_sort.py
+++ src/sorting/insertion_sort.py
@@ -1,2 +1,14 @@
-def insertion_sort():
-    return
+def insertion_sort(arr, left, right):
+    comparisons = 0 
+    for i in range(left + 1, right + 1): 
+        key = arr[i]
+        j = i - 1 
+        while j >= left:
+            comparisons += 1
+            if arr[j] > key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            else:
+                break
+        arr[j + 1] = key
+    return comparisons
